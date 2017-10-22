819d3f3db0c53edaf3b6044822bd2a13d91c623d
tsuto <suto.tadashi@gmail.com>
2017-10-22 21:23:43 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+﻿(function(ext) {
+    ext._shutdown = function() {};
+
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.test = function(str) {
+        /* ボタンが押されたときの処理を書く */
+    };
+
+    var descriptor = {
+        blocks: [
+            /* ブロックの定義を書く */
+        ]
+    };
+
+    ScratchExtensions.register('Test', descriptor, ext);
+})({});
